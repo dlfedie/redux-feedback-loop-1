ec680e6c154c63a8fd08df2cfa00f4468dc158bf
Marit Zelinsky <maritzelinsky@me.com>
2019-08-25 12:28:20 -0500

    moves header into its own component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,13 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <h1>App</h1>
     );
   }
 }
